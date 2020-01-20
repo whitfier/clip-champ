0473d1d2daa62cc357d13274ea160098811d86c4
Ryan Whitfield <whitryan@amazon.com>
2020-01-19 18:50:29 -0800

    Updates

---
+++ precache-manifest.61cb8cc0f5732633d7dd22a3c765f9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7880d580edbcd4e6526452851c3418",
+    "url": "/clip-champ/index.html"
+  },
+  {
+    "revision": "37c0610ce6828b6fe9d4",
+    "url": "/clip-champ/static/css/2.4ddf7b73.chunk.css"
+  },
+  {
+    "revision": "b5d2a7066ddbfb2af68f",
+    "url": "/clip-champ/static/css/main.7959b2ad.chunk.css"
+  },
+  {
+    "revision": "37c0610ce6828b6fe9d4",
+    "url": "/clip-champ/static/js/2.f530892a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/clip-champ/static/js/2.f530892a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5d2a7066ddbfb2af68f",
+    "url": "/clip-champ/static/js/main.ed3d3023.chunk.js"
+  },
+  {
+    "revision": "1a762dbd37568b273bed",
+    "url": "/clip-champ/static/js/runtime-main.164f91dd.js"
+  }
+]);
